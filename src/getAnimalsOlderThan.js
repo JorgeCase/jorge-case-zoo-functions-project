678107b3409bd17539b70c9716385f85eaceb0b5
JorgeKz <jorgekzbra@gmail.com>
2022-05-20 19:00:18 -0300

    Implementando requisito 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species
+    .find((getAnimal) => getAnimal.name === animal)
+    .residents.every((element) => element.age >= age);
 }
+
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
